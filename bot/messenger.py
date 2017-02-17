aacc583cefada4018f8eea59577cbe7e33d66106
god-hirosi <hirosi.god@gmail.com>
2017-02-18 02:02:35 +0900

    a
    a

--- bot/messenger.py
+++ bot/messenger.py
@@ -135,7 +135,7 @@ class Messenger(object):
     def purpose_check(self, channel_id):
         self.send_message(channel_id, '何を調べたい？ グルメ or 体験から選んでね！'.decode('utf-8'))
     
-    def write_params(self, a, b, c, d):
+    def write_param(self, a, b, c, d):
         txt = str(len(a)) + ',' + str(len(b)) + ',' + str(c) + ',' + str(d)
         self.send_message(channel_id, txt.decode('utf-8'))
         
